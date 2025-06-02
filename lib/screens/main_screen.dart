71476eba90129227efb28ca92533434938dae314
amandanalesso <nalessoamanda@gmail.com>
2025-06-02 16:35:49 -0300

    bottom navbar working

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+import 'pet_profile.dart';
+import 'vaccine_form.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomeScreen(),
+    const PetProfileScreen(),
+    const VaccineFormScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.vaccines),
+            label: 'Vacinas',
+          ),
+        ],
+      ),
+    );
+  }
+}
